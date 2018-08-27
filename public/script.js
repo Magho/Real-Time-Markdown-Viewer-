a519b2ae196069c1749b4c23f3fa10c001af6167
Magho <mmaghraby134@gmail.com>
2018-08-27 18:55:06 +0200

    solve tabButton error

--- public/script.js
+++ public/script.js
@@ -1,13 +1,36 @@
-
 /* public/script.js */
 
 window.onload = function() {
     var converter = new showdown.Converter();
     var pad = document.getElementById('pad');
-    var markdownArea = document.getElementById('markdown');   
+    var markdownArea = document.getElementById('markdown'); 
+
+    // make the tab act like a tab
+    pad.addEventListener('keydown',function(e) {
+        if(e.keyCode === 9) { // tab was pressed
+            // get caret position/selection
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            var target = e.target;
+            var value = target.value;
+
+            // set textarea value to: text before caret + tab + text after caret
+            target.value = value.substring(0, start)
+                            + "\t"
+                            + value.substring(end);
+
+            // put caret at right position again (add one for the tab)
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent the focus lose
+            e.preventDefault();
+        }
+    });
 
     var previousMarkdownValue;          
 
+    // convert text area to markdown html
     var convertTextAreaToMarkdown = function(){
         var markdownText = pad.value;
         previousMarkdownValue = markdownText;
@@ -22,14 +45,17 @@ window.onload = function() {
         return false;
     };
 
+    // check every second if the text area has changed
     setInterval(function(){
         if(didChangeOccur()){
             convertTextAreaToMarkdown();
         }
     }, 1000);
 
+    // convert textarea on input change
     pad.addEventListener('input', convertTextAreaToMarkdown);
-     // ignore if on home page
+
+    // ignore if on home page
     if(document.location.pathname.length > 1){
         // implement share js
         var documentName = document.location.pathname.substring(1);
@@ -38,5 +64,8 @@ window.onload = function() {
             convertTextAreaToMarkdown();
         });        
     }
+
+    // convert on page load
     convertTextAreaToMarkdown();
+
 };
